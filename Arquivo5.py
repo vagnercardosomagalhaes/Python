65855fca1c5c560a8335d18edd26099acca5d064
vagnercardoso <contato.vagner.cardoso@gmail.com>
2024-03-08 13:44:52 -0300

    Atualização 08032024 13:44

---
+++ Arquivo5.py
@@ -0,0 +1,22 @@
+#qtd = 0
+#while qtd < 3:
+#    usr = input("Digite usuario: ")
+#    psw = input("Digite a senha: ")       
+#    if psw == "teste" and usr == "vagner":
+#        for qtd in range(1, 7):
+#            print("Senha correta")
+#        break
+#    else:
+#        print("senha errada")
+#        
+#    qtd += 1
+#
+#print("Fim")
+#print(f'{usr: >10}')
+#print(f'{usr: <10}.')
+#print(f'{usr:#^10}')
+
+nome = 'ABCDE FGHIJ LMNOP QRST UVXZ'
+print(nome[2]) # pega caracter da posição 2 da string -> S
+print(nome[2:9]) # pega posicao inicial e final da string - fatiando a string (como a substring do c#)
+print( len(nome))
---
+++ Arquivo6.py
@@ -0,0 +1,8 @@
+opcao = 0 # flag
+numero = int(input("Digite um numero"))
+print(f'O dobro do {numero} é {numero * 2:.2f}')
+
+if condicao:
+    opcao = 1
+else:
+    opcao = 2
