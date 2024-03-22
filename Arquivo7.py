ce8e4d38eeeae19c51a8c343ff8cd26ecaafa577
vagnercardoso <contato.vagner.cardoso@gmail.com>
2024-03-22 12:12:07 -0300

    Atualização de aulas do curso

---
+++ Arquivo7.py
@@ -0,0 +1,25 @@
+#def Imprimir():
+#    print("Teste de função")
+#    print("Teste de função")
+#    print("Teste de função")
+#    print("Teste de função")
+
+#nome = 'Vagner'
+#idade = 47
+#peso = 90
+
+#def Imprimir(nome, idade, peso):
+#    print(nome,idade,peso)
+    
+
+#Imprimir(nome,idade,peso)
+
+#def soma(x,y):
+#    print(x + y)
+#
+#x = 100
+#y = 44000
+#
+#soma(x,y)
+
+    
